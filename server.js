2de271551d8e6f66543f9d33966b2144feda2835
Viren Gajjar <vgajjar1010@gmail.com>
2024-12-27 14:01:16 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const myServer = http.createServer((req, res) => {
+    const date = new Date().toLocaleDateString();
+    const logtext = date + ': request recieved\n';
+    fs.appendFile('log.txt', logtext, (err) => { });
+   
+    const parsedUrl = url.parse(req.url, true);
+    console.log(parsedUrl);
+
+    if (req.url === '/favicon.ico') {
+        res.end();
+    }
+    else if (req.url === '/') {
+        res.end("Welcome to home page")
+    }
+    else if (req.url === '/about') {
+        res.end("this is viren here")
+    } else {
+        res.end("Darshan69 not found - 404");
+    }
+})
+
+myServer.listen(8000, () => {
+    console.log("server started");
+})
